74cbba93282211bc8936787d2e794683ce591ea8
ruslan-kornich <korny4rus143@gmail.com>
2023-08-08 13:28:08 +0300

    visible markers

--- script.py
+++ script.py
@@ -4,7 +4,7 @@ import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "map_activity.settings")
 django.setup()
 
-from activity.models import Marker
+from activity.models import Marker, Activity
 import random
 from faker import Faker
 
@@ -14,20 +14,33 @@ fake = Faker()
 min_lat, max_lat = 47.75, 48.75  # latitude
 min_lon, max_lon = 33.0, 35.5  # longitude
 
-# Coordinates for specific cities
-cities = [
-    {'name': 'Днепр', 'latitude': 48.4593, 'longitude': 35.0386},
-    {'name': 'Запорожье', 'latitude': 47.8388, 'longitude': 35.1396},
-    {'name': 'Харьков', 'latitude': 49.9935, 'longitude': 36.2304},
-]
+# Create a list of activities
+activities = ['Non-Food Items (NFI)', 'Evacuation', 'Water Distribution', 'Bread']
 
-for city in cities:
-    lat = city['latitude']
-    lon = city['longitude']
-    Marker.objects.create(
-        location=[float(lat), float(lon)],
-        activity='Water distribution',
-        quantity=random.randint(10, 100),
-        date=fake.date_between(start_date='-1y', end_date='today'),
-        place=city['name']
-    )
+
+# Coordinates for random markers
+def generate_random_coordinates():
+    return [round(random.uniform(min_lat, max_lat), 6), round(random.uniform(min_lon, max_lon), 6)]
+
+
+# Function to add test markers
+def add_test_markers():
+    for _ in range(50):  # You can change the number of markers you want to create (50 in this case)
+        activity_name = random.choice(activities)
+        activity = Activity.objects.get(name=activity_name)
+        quantity = random.randint(10, 100)
+        date = fake.date_between(start_date='-1y', end_date='today')
+        location = generate_random_coordinates()
+        place = fake.city()
+
+        Marker.objects.create(
+            activity=activity,
+            quantity=quantity,
+            date=date,
+            location=location,
+            place=place
+        )
+
+
+if __name__ == "__main__":
+    add_test_markers()
--- static/js/main.js
+++ static/js/main.js
@@ -1,109 +1,112 @@
 var map = L.map('map').setView([48.5, 35], 7);
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
 
-    // Загрузка данных маркеров через API
-    fetch('/api/markers/')
-        .then(response => response.json())
-        .then(data => {
-            // Создаем объект для хранения информации о маркерах с одинаковым местоположением и активностью
-            var markerDataByLocationAndActivity = {};
+var markers = []; // Хранилище для маркеров
+var activityLayers = {}; // Хранилище для слоев активностей
+var markerDataByLocationAndActivity = {}; // Хранилище для данных маркеров по местоположению и активности
 
-            // Получаем все уникальные активности для фильтрации
-            var uniqueActivities = new Set(data.map(marker => marker.activity.name));
+// Загрузка данных маркеров через API
+fetch('/api/markers/')
+    .then(response => response.json())
+    .then(data => {
+        var uniqueActivities = new Set(data.map(marker => marker.activity.name));
 
-            // Добавляем чекбоксы для выбора активностей в фильтре
-            var activityCheckboxes = document.getElementById('activity-checkboxes');
-            uniqueActivities.forEach(activity => {
-                var checkboxContainer = document.createElement('label');
-                checkboxContainer.innerHTML = `<input type="checkbox" value="${activity}" checked> ${activity}<br>`;
-                activityCheckboxes.appendChild(checkboxContainer);
-            });
+        var activityCheckboxes = document.getElementById('activity-checkboxes');
+        uniqueActivities.forEach(activity => {
+            var checkboxContainer = document.createElement('label');
+            checkboxContainer.innerHTML = `<input type="checkbox" value="${activity}" checked> ${activity}<br>`;
+            activityCheckboxes.appendChild(checkboxContainer);
+        });
 
-            // Обработчик события изменения выбранных активностей в фильтре
-            var checkboxes = document.querySelectorAll('#activity-checkboxes input[type="checkbox"]');
-            checkboxes.forEach(checkbox => {
-                checkbox.addEventListener('change', function() {
-                    var selectedActivities = Array.from(checkboxes)
-                        .filter(checkbox => checkbox.checked)
-                        .map(checkbox => checkbox.value);
+        var checkboxes = document.querySelectorAll('#activity-checkboxes input[type="checkbox"]');
+        checkboxes.forEach(checkbox => {
+            checkbox.addEventListener('change', function () {
+                var selectedActivities = Array.from(checkboxes)
+                    .filter(checkbox => checkbox.checked)
+                    .map(checkbox => checkbox.value);
 
-                    // Перебираем все точки на карте и устанавливаем видимость в зависимости от выбранных активностей
-                    data.forEach(marker => {
-                        var lat = marker.location[0];
-                        var lon = marker.location[1];
-                        var activity = marker.activity.name;
-                        var isVisible = selectedActivities.includes(activity);
-                        var key = lat.toString() + lon.toString() + activity;
-
-                        map.eachLayer(layer => {
-                            if (layer instanceof L.Marker && layer.options.icon.options.markerColor === marker.activity.color) {
-                                if (isVisible) {
-                                    layer.setOpacity(1);
-                                } else {
-                                    layer.setOpacity(0);
-                                }
-                            }
-                        });
-                    });
-                });
-            });
-
-            // Обрабатываем каждую запись из полученных данных и добавляем маркеры на карту
-            data.forEach(marker => {
-                var lat = marker.location[0];
-                var lon = marker.location[1];
-                var activity = marker.activity.name;
-                var quantity = marker.quantity;
-                var date = marker.date;
-                var place = marker.place;
-                var iconClass = marker.activity.icon; // Иконка для активности из базы данных
-                var color = marker.activity.color; // Цвет для активности из базы данных
-
-                // Создаем иконку с использованием AwesomeMarkers
-                var icon = L.AwesomeMarkers.icon({
-                    icon: iconClass, // Иконка для активности из базы данных
-                    prefix: 'fa', // Префикс класса иконок Font Awesome
-                    markerColor: color // Цвет маркера для активности из базы данных
-                });
-
-                // Добавляем маркер на карту и обработчик клика
-                var markerPopup = L.popup().setContent(`<b>${place}</b><br>Activity: ${activity}<br>Quantity: ${quantity}<br>Date: ${date}`);
-                L.marker([lat, lon], { icon: icon }).addTo(map).bindPopup(markerPopup).on('click', function() {
-                    // Отображаем информацию о маркерах в боковом меню
-                    var markerInfoList = document.getElementById('marker-list');
-                    markerInfoList.innerHTML = ''; // Очищаем список
-
-                    // Получаем все записи с данным ключом и добавляем их в список
-                    var markerDataList = markerDataByLocationAndActivity[key];
-                    markerDataList.forEach(markerData => {
-                        var listItem = document.createElement('li');
-                        listItem.innerHTML = `<b>${markerData.place}</b><br>Activity: ${markerData.activity}<br>Quantity: ${markerData.quantity}<br>Date: ${markerData.date}`;
-                        markerInfoList.appendChild(listItem);
-                    });
-                });
-
-                // Формируем уникальный ключ на основе местоположения и активности
-                var key = lat.toString() + lon.toString() + activity;
-
-                // Если такой ключ уже существует, добавляем информацию в объект
-                if (key in markerDataByLocationAndActivity) {
-                    markerDataByLocationAndActivity[key].push({
-                        activity: activity,
-                        quantity: quantity,
-                        date: date,
-                        place: place
-                    });
-                } else {
-                    // Если ключа еще нет, создаем новую запись в объекте
-                    markerDataByLocationAndActivity[key] = [{
-                        activity: activity,
-                        quantity: quantity,
-                        date: date,
-                        place: place
-                    }];
-                }
+                updateMarkerVisibility(selectedActivities);
             });
         });
+
+        data.forEach(marker => {
+            var lat = marker.location[0];
+            var lon = marker.location[1];
+            var activity = marker.activity.name;
+            var quantity = marker.quantity;
+            var date = marker.date;
+            var place = marker.place;
+            var iconClass = marker.activity.icon; // Иконка для активности из базы данных
+            var color = marker.activity.color; // Цвет для активности из базы данных
+
+            // Создаем иконку с использованием AwesomeMarkers
+            var icon = L.AwesomeMarkers.icon({
+                icon: iconClass, // Иконка для активности из базы данных
+                prefix: 'fa', // Префикс класса иконок Font Awesome
+                markerColor: color // Цвет маркера для активности из базы данных
+            });
+
+            // Добавляем маркер на слой активности, вместо добавления его прямо на карту
+            if (!activityLayers[activity]) {
+                activityLayers[activity] = L.layerGroup().addTo(map);
+            }
+
+            var markerPopup = L.popup().setContent(`<b>${place}</b><br>Activity: ${activity}<br>Quantity: ${quantity}<br>Date: ${date}`);
+            var markerInstance = L.marker([lat, lon], { icon: icon }).addTo(activityLayers[activity]).bindPopup(markerPopup);
+            markerInstance.on('click', function () {
+                // Отображаем информацию о маркерах в боковом меню
+                var markerInfoList = document.getElementById('marker-list');
+                markerInfoList.innerHTML = ''; // Очищаем список
+
+                // Получаем все записи с данным ключом и добавляем их в список
+                var key = lat.toString() + lon.toString() + activity;
+                var markerDataList = markerDataByLocationAndActivity[key];
+                markerDataList.forEach(markerData => {
+                    var listItem = document.createElement('li');
+                    listItem.innerHTML = `<b>${markerData.place}</b><br>Activity: ${markerData.activity}<br>Quantity: ${markerData.quantity}<br>Date: ${markerData.date}`;
+                    markerInfoList.appendChild(listItem);
+                });
+            });
+
+            markers.push({ marker: markerInstance, activity, isVisible: true });
+
+            // Формируем уникальный ключ на основе местоположения и активности
+            var key = lat.toString() + lon.toString() + activity;
+
+            // Если такой ключ уже существует, добавляем информацию в объект
+            if (key in markerDataByLocationAndActivity) {
+                markerDataByLocationAndActivity[key].push({
+                    activity: activity,
+                    quantity: quantity,
+                    date: date,
+                    place: place
+                });
+            } else {
+                // Если ключа еще нет, создаем новую запись в объекте
+                markerDataByLocationAndActivity[key] = [{
+                    activity: activity,
+                    quantity: quantity,
+                    date: date,
+                    place: place
+                }];
+            }
+        });
+
+        updateMarkerVisibility = function (selectedActivities) {
+            // Удаляем все слои активности
+            for (var activity in activityLayers) {
+                map.removeLayer(activityLayers[activity]);
+            }
+
+            // Добавляем выбранные слои активности обратно
+            selectedActivities.forEach(activity => {
+                if (activity in activityLayers) {
+                    map.addLayer(activityLayers[activity]);
+                }
+            });
+        };
+    })
+    .catch(error => console.error('Error fetching markers:', error));
